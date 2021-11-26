b599021b3da457a6e760ab6a709b633d0f016e5d
Walter Gauto <waltergautofcb@gmail.com>
2021-11-25 21:34:10 -0300

    Se agrega el modelo Producto en model/Producto.java

---
+++ app/src/main/java/com/example/tp2_android_wfe/model/Producto.java
@@ -0,0 +1,42 @@
+package com.example.tp2_android_wfe.model;
+
+public class Producto {
+
+    private Integer codigo;
+    private String nombre;
+    private Integer precio;
+    private Integer existencia;
+
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Integer precio) {
+        this.precio = precio;
+    }
+
+    public Integer getExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(Integer existencia) {
+        this.existencia = existencia;
+    }
+}
